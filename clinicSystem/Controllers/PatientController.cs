1aa6287ecea1ea3db2074a5f9e2522aa71db961f
Khloud Hisham <khokhakammar@gmail.com>
2025-10-26 18:32:32 +0300

    Update PatientController logic and actions

--- clinicSystem/Controllers/PatientController.cs
+++ clinicSystem/Controllers/PatientController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using clinicSystem.Models.Entities;
+using clinicSystem.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace clinicSystem.Controllers
 {
     public class PatientController : Controller
     {
-        public IActionResult Index()
+        private readonly IGenericRepository<Patient> _repository;
+        public PatientController(IGenericRepository<Patient> repository)
         {
-            return View();
+            _repository = repository;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var patients = await _repository.GetAllAsync();
+            return View(patients);
+        }
+        public IActionResult New()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveNew(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.AddAsync(patient);
+                return RedirectToAction("Index");
+            }
+            return View("Create", patient);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.DeleteAsync(id);
+            return RedirectToAction("Index");
+        }
+
     }
 }
