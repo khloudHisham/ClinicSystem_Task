6d46da64797f21fb71e8ed59086dd86402b513cb
Khloud Hisham <khokhakammar@gmail.com>
2025-10-26 16:54:17 +0300

    installing another 2 packages for design & code generation.design

--- clinicSystem/obj/Debug/net8.0/clinicSystem.AssemblyInfo.cs
+++ clinicSystem/obj/Debug/net8.0/clinicSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("clinicSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1806d5cff06f883328687b0e6600a1052dadc14a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd733cbdaec7d9f384b4b016a87a5544d4f67d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("clinicSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("clinicSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
