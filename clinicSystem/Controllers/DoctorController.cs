190e3218a733d65e3878788fef0f760a74c1a923
Khloud Hisham <khokhakammar@gmail.com>
2025-10-26 17:58:12 +0300

    Update DoctorController logic and actions

--- clinicSystem/Controllers/DoctorController.cs
+++ clinicSystem/Controllers/DoctorController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using clinicSystem.Models.Data;
+using clinicSystem.Models.Entities;
+using clinicSystem.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace clinicSystem.Controllers
 {
     public class DoctorController : Controller
     {
-        public IActionResult Index()
+        private readonly IGenericRepository<Doctor> _repository;
+        private readonly ApplicationDbContext _context;
+        public DoctorController(IGenericRepository<Doctor> repository, ApplicationDbContext context)
         {
-            return View();
+            _repository = repository;
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var doctors = await _repository.GetAllAsync();
+            return View(doctors);
+        }
+
+        public IActionResult New()
+        {
+            ViewBag.Clinics = new SelectList(_context.Clinics, "Id", "Name");
+            return View("Create");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveNew(Doctor doctor)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.AddAsync(doctor);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Clinics = new SelectList(_context.Clinics, "Id", "Name");
+            return View("Create", doctor);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.DeleteAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
