0ab8d2edd8d69f72469d86775b6687580e6e7ea4
Khloud Hisham <khokhakammar@gmail.com>
2025-10-26 18:37:27 +0300

    Update ScheduleController logic and actions

--- clinicSystem/Controllers/ScheduleController.cs
+++ clinicSystem/Controllers/ScheduleController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using clinicSystem.Models.Data;
+using clinicSystem.Models.Entities;
+using clinicSystem.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace clinicSystem.Controllers
 {
     public class ScheduleController : Controller
     {
-        public IActionResult Index()
+        private readonly IGenericRepository<Schedule> _repository;
+        private readonly ApplicationDbContext _context;
+        public ScheduleController(IGenericRepository<Schedule> repository, ApplicationDbContext context)
         {
-            return View();
+            _repository = repository;
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var schedules = await _repository.GetAllAsync();
+            return View(schedules);
+        }
+
+        public IActionResult New()
+        {
+            ViewBag.Doctors = new SelectList(_context.Doctors, "Id", "Name");
+            return View("Create");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveNew(Schedule schedule)
+        {
+            if (ModelState.IsValid)
+            {
+                await _repository.AddAsync(schedule);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Doctors = new SelectList(_context.Doctors, "Id", "Name");
+            return View("Create", schedule);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.DeleteAsync(id);
+            return RedirectToAction("Index");
         }
     }
 }
